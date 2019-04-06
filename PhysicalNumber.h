8391d54aa3ba0a80d40ee77740f074b0cd9acc1c
FerasTr <daragma.feras@gmail.com>
2019-04-06 14:32:18 +0300

    Added operators sign

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -14,14 +14,47 @@ private:
 
 public:
   PhysicalNumber(double value, int unit);
-
-  friend PhysicalNumber operator+(const PhysicalNumber &b);
-  friend PhysicalNumber operator-(const PhysicalNumber &b);
-  PhysicalNumber &operator+=(const PhysicalNumber &other);
-  PhysicalNumber &operator-=(const PhysicalNumber &b);
-  friend PhysicalNumber operator++(const PhysicalNumber &b);
-  friend PhysicalNumber operator--(const PhysicalNumber &b);
-  friend istream &operator>>(istream &in, PhysicalNumber &vec);
-  friend ostream &operator<<(ostream &out, const PhysicalNumber &vec); // output
+  /*** Arithmetic Operators ***/
+  // +
+  PhysicalNumber operator+(const PhysicalNumber &pnum);
+  // -
+  PhysicalNumber operator-(const PhysicalNumber &pnum);
+  /*** Unary Operators ***/
+  // unary +
+  const PhysicalNumber operator+() const;
+  // unary -
+  const PhysicalNumber operator-() const;
+  /*** Post/Pre Operators ***/
+  // post ++
+  const PhysicalNumber operator++(int);
+  // post --
+  const PhysicalNumber operator--(int);
+  // ++ pre
+  PhysicalNumber &operator++();
+  // -- pre
+  PhysicalNumber &operator--();
+  /** Assignment Operators ***/
+  // +=
+  PhysicalNumber &operator+=(const PhysicalNumber &pnum);
+  // -=
+  PhysicalNumber &operator-=(const PhysicalNumber &pnum);
+  /*** Relational operators ***/
+  // ==
+  friend bool operator==(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  // !=
+  friend bool operator!=(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  // <
+  friend bool operator>(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  // >
+  friend bool operator<(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  // >=
+  friend bool operator>=(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  // <=
+  friend bool operator<=(const PhysicalNumber &lhs, const PhysicalNumber &rhs);
+  /*** Stream Operators ***/
+  // <<
+  friend ostream &operator<<(ostream &out, const PhysicalNumber &pnum);
+  // >>
+  friend istream &operator>>(istream &in, PhysicalNumber &pnum);
 };
 } // namespace ariel
