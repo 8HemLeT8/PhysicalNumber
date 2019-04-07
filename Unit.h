a0f8000f644e18839bbc9a06de83bee3b14cf45d
FerasTr <32588570+FerasTr@users.noreply.github.com>
2019-04-07 14:11:58 +0300

    Update Unit.h

--- Unit.h
+++ Unit.h
@@ -1,22 +1,22 @@
+#pragma once
 #include <iostream>
 #include <cstdio>
+
+
 
 namespace ariel
 {
 
 enum Unit
 {
-    CM,
-    M,
-    KM,
-    SEC,
-    MIN,
-    HOUR,
-    G,
-    KG,
-    TON
+  CM,
+  M,
+  KM,
+  SEC,
+  MIN,
+  HOUR,
+  G,
+  KG,
+  TON
 };
-
-long multby[] = {1, 100, 1000, 1, 60, 3600, 1, 1000, 1000000};
-
 } // namespace ariel
